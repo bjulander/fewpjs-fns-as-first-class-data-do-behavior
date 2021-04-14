f126367ad96cb8f7e778ef814ad4391569dbcf6d
Blake Julander <13lakejulander@gmail.com>
2021-04-14 12:40:27 -0700

    commit tree 3fdd3dac0ba71c2221bff40c0f6fb9cab3ee29e0

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeS){
+  const splitT = timeS.replace(/:/g,'')
+  const hour = parseInt(splitT, 10);
+  if ( hour <= 1200 && hour >= 100) return "Good Morning"
+  if ( hour >= 1700 && hour <= 2400) return "Good Evening"
+  if ( hour >= 2500 || hour < 100 ) return "Please enter a valid time"
+  return "Good Afternoon"
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg;
+}
